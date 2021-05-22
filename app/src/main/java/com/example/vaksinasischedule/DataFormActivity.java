91297f3f2180015f7881c78ff688ed584a9e7088
Brahma Aditama <aditama.brahma@gmail.com>
2021-05-22 11:31:47 +0700

    Lengkapi Data form

---
+++ app/src/main/java/com/example/vaksinasischedule/DataFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.vaksinasischedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_form);
+    }
+}
